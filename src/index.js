0cb93eae0a5fe7f6340fda68a7045654ed094d64
droogadulce <ahigareda.mayra@gmail.com>
2019-09-21 11:05:20 -0500

    Primer problema resuelto

--- src/index.js
+++ src/index.js
@@ -1,24 +1,44 @@
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
-    }, time);
+    if (!time || !product || !table) {
+      reject(
+        new Error(
+          'Falta alguno de los siguientes datos para preparar la orden: time, product o table'
+        )
+      );
+    } else {
+      setTimeout(() => {
+        resolve(
+          `=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`
+        );
+      }, time);
+    }
   });
-}
+};
 
 const menu = {
   hamburger: 'Combo Hamburguesa',
   hotdog: 'Combo Hot Dogs',
-  pizza: 'Combo Pizza',
+  pizza: 'Combo Pizza'
 };
 
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+  orders(randomTime(), menu.hamburger, table[3])
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
 };
+
+/**
+ * Retorna un valor en mili segundos de forma aleatoria en el rango de
+ * 1000ms hasta 8000ms.
+ */
+function randomTime() {
+  const MAX = 9000; // excluded
+  const MIN = 1000;
+  return Math.floor(Math.random() * (MAX - MIN)) + MIN;
+}
 
 waiter();
