8bd999b76246812b6863aa13abf4387697a2129e
Santiago Jimenez <sjimen13@gmail.com>
2019-09-21 11:06:17 -0500

    randomTime

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ const orders = (time, product, table) => {
     }, time);
   });
 }
+function randomTime (){
+  const min = 1000;
+  const max = 8000;
+  var numberRandom =(Math.random()*(max-min)+ min).toFixed(0)
+  return numberRandom  ;
+}
+
 
 const menu = {
   hamburger: 'Combo Hamburguesa',
@@ -16,7 +23,13 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(randomTime(), menu.hamburger, table[3])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+};
+
+const waiter2 = () => {
+  orders(randomTime(), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
