5f7415c088c5e5d1479937736646515b994102da
Jesús Antonio García Valadez <jesus.garciav@me.com>
2019-09-21 10:48:48 -0500

    feat: add a new waiter who is in charge to serve table 1 and table 3

--- src/index.js
+++ src/index.js
@@ -37,3 +37,15 @@ const waiter = () => {
 };
 
 waiter();
+
+const waiter2 = () => {
+  orders(randomTime(), menu.hotdog, table[0])
+      .then((res) => {
+        console.log(res)
+        return orders(randomTime(), menu.pizza, table[2])
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.error(err));
+};
+
+waiter2()
