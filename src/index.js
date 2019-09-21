7f0357acee2f03625502bd297af2f448ea364e49
Miguel Hernandez <miguel.tks@gmail.com>
2019-09-21 12:06:38 -0500

    Problema 2 - terminado

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
-    if (!time || !product || !table || time > 8000) {
+    if (!time || !product || !table) {
       reject(
-        `=== Error al realizar el pedido tiempo: ${time} producto: ${product} mesa: ${table}`
+        `=== Error al realizar el pedido, valor erroneo tiempo: ${time} producto: ${product} mesa: ${table}`
       );
+    } else if (isNaN(time)) {
+      reject(`=== El valor del tiempo no es numerico tiempo: ${time}`);
+    } else if (time > 8000) {
+      reject(`=== El tiempo del pedido es mayor al esperado tiempo: ${time}`);
     }
     setTimeout(() => {
       resolve(
@@ -23,7 +27,7 @@ const menu = {
 const table = ["Mesa 1", "Mesa 2", "Mesa 3", "Mesa 4", "Mesa 5"];
 
 function randomTime() {
-  return Math.floor(Math.random() * 8000 + 1000);
+  return Math.floor(Math.random() * 7000 + 1000);
 }
 
 const waiter = () => {
@@ -32,4 +36,18 @@ const waiter = () => {
     .catch(err => console.error(err));
 };
 
+const waiter2 = () => {
+  orders(randomTime(), menu.hotdog, table[0])
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => console.error(err))
+    .then(() => {
+      orders(randomTime(), menu.pizza, table[2])
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+    });
+};
+
 waiter();
+waiter2();
