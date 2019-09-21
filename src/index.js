29fad06493411a9182a8f82517e1d4cde8d50fa2
Francisco Angel <paconity@gmail.com>
2019-09-21 15:02:09 -0500

    Inicio de reto

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+const randomTime = (min, max) => {
+  return time = Math.floor(Math.random() * (max - min) + min);
+}
+
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
+      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time/1000} segundos para la ${table}`);
     }, time);
   });
 }
@@ -16,9 +20,12 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(randomTime(1000, 8000), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
 
 waiter();
+
+
+
