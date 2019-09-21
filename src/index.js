bbc2625d2f1d58a946111c7e931c5ccdea7a7bc5
David Behar <davbelom@gmail.com>
2019-09-21 12:15:47 -0500

    Reto1 - randomnumber

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-const orders = (time, product, table) => {
+/* eslint-disable no-console */
+const getOrders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
     }, time);
   });
+};
+function randomTime() {
+  const max = 8000;
+  const min = 1000;
+  return Math.round(Math.random() * (max - min) + min);
 }
 
 const menu = {
@@ -16,9 +22,28 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  getOrders(randomTime(), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
+
+const waiter2 = () => {
+  const orders = [
+    // En mi arreglo de mesas me cree una mesa 0 para que funcione con lenguaje natural las mesas
+    {
+      table: table[1],
+      order: hotdog,
+    },
+    {
+      table: table[3],
+      order: pizza,
+    },
+  ];
+  getOrders(randomTime(), orders[0].order, orders[0].table)
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+};
+
+console.log(randomTime());
 
 waiter();
