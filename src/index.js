bc18a47b6e1ed91a17e4dff3e5376197d3f27ac5
Mauricio Fajardo <mauriciositel@hotmail.com>
2019-09-21 11:43:33 -0500

    Primera versión incluyendo randomTime

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const randomTime = ()=> Math.floor((Math.random() * 8000) + 1000)
+
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
@@ -16,9 +18,11 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(randomTime(), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
 
 waiter();
+
+orders(randomTime(),menu.hamburger,table[0])
