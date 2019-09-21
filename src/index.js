3581ade3e31ca64ba5d0a4965da3c572d62dca9e
Kristian Lopez <kristian.lom@gmail.com>
2019-09-21 11:02:27 -0500

    Solución del primer reto.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve, _) => {
     setTimeout(() => {
       resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
     }, time);
@@ -13,12 +13,19 @@ const menu = {
   pizza: 'Combo Pizza',
 };
 
-const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
+const tables = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(randomTime(), menu.hamburger, tables[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
 
 waiter();
+
+function randomTime() {
+    const min = 1000
+    const max = 8000
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
