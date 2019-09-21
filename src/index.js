037d1f5d78290650d067793a139714b8e6954545
Heladito <nicolasestevanprieto@gmail.com>
2019-09-21 11:24:11 -0500

    Reto echo

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const orders = (time, product, table) => {
   });
 }
 
+const randomTime = () => {
+  return Math.floor((Math.random() * 8000) + 1000)
+} 
+
 const menu = {
   hamburger: 'Combo Hamburguesa',
   hotdog: 'Combo Hot Dogs',
@@ -16,9 +20,40 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
-    .then((res) => console.log(res))
+  orders(randomTime(), menu.hamburger, table[3])
+  
+    .then((res) => {console.log(res); })
     .catch((err) => console.error(err));
 };
 
+const waiter2 = () => {
+  orders(randomTime(), menu.pizza, table[2])
+  .then((res) => {console.log(res); return orders(randomTime(), menu.hotdog, table[0])})
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
+};
+
 waiter();
+waiter2();
+const waiter3 = async () => {
+  try {
+  const respuesta = await orders(randomTime(), menu.hotdog, table[1]);
+  const respuesta2 = await orders(randomTime(), menu.pizza, table[1]);
+  const respuesta3 = await orders(randomTime(), menu.hotdog, table[1]);
+  console.log(respuesta);
+  console.log(respuesta2);
+  console.log(respuesta3);
+  }
+  catch(error) {
+    console.error(error);
+  }
+};
+
+waiter3();
+
+
+
+
+
+
+
