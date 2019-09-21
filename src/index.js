0e97c429c4f65d4b7b1042045c5834b3ceb037bf
Diego Cruz <abeja.dact_@hotmail.com>
2019-09-21 11:48:39 -0500

    solucion del reto 4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
+let randomTime = (min,max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
-    }, time);
+    if(time > 0) {
+      setTimeout(() => {
+        resolve (`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
+      }, time);
+    }
+    else {
+      reject (`No se recibio ningun pedido`)
+    }
+    
   });
 }
 
@@ -16,9 +26,32 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+  orders(randomTime(1000,8000) , menu.hamburger, table[3])
+    .then((resolve) => console.log(resolve))
+    .catch((reject) => console.error(reject));
 };
 
+const waiter2 = () => {
+  orders(randomTime(1000,8000) , menu.hotdog, table[0])
+    .then((resolve) => {
+      console.log(resolve);
+    return orders(randomTime(1000,8000) , menu.pizza, table[2])
+    .then((resolve) => console.log(resolve))
+    .catch((reject) => console.error(reject));
+    })
+    .catch((reject) => console.error(reject));
+    
+}
+
+const  waiter3 = async() => {
+   const order1 = await orders(randomTime(1000,8000) , menu.hotdog, table[0])
+   const  order2 = await orders(randomTime(1000,8000) , menu.pizza, table[2])
+  console.log( `${order1} \n ${order2}`)
+ 
+};
+
+
 waiter();
+waiter2();
+waiter3();
+
