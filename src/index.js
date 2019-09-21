531ea63e9d7c913b680bcfb291be16aa8bb2c049
Omar Flores Grimontt <omar.flores@3dves.com>
2019-09-21 12:28:46 -0500

    challenge one, two, three

--- src/index.js
+++ src/index.js
@@ -1,24 +1,110 @@
+const randomTime = () => Math.round(Math.random() * (8000 - 1000) + 1000)
+
+const errorHandler = (time, product, table) => {
+  if (!time || !typeof time === 'int') {
+    return 'No tenemos un tiempo para su pedido'
+  }
+
+  if (!table || !tables.includes(table)) {
+    if (table) {
+      return `Lo sentimos, no tenemos la mesa ${table} disponible`
+    }
+    return 'Lo sentimos, no podemos atenderle si no nos pide una mesa'
+  }
+
+  if (!product || !Object.keys(menu).includes(product)) {
+    if (product) {
+      return `El producto ${product} no se encuentra disponible`
+    }
+    return 'Lo sentimos, no podemos servirle si no especifica su pedido'
+  }
+
+  return false
+}
+
+
 const orders = (time, product, table) => {
-  console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
-    }, time);
-  });
+      const error = errorHandler(time, product, table)
+
+      if (error) {
+        reject(error)
+      }
+
+      resolve(`Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table} \n`)
+    }, time)
+  })
 }
 
 const menu = {
   hamburger: 'Combo Hamburguesa',
   hotdog: 'Combo Hot Dogs',
   pizza: 'Combo Pizza',
-};
+}
 
-const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
+const tables = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5']
 
-const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
-};
+const waiters = async (indications) => {
+  const time = randomTime()
+  const allOrders = []
+  let res
+  for (indication of indications) {
+    const { food, table } = indication
+    for (order of food) {
+      try {
+        res = await orders(time, order, tables[table])
+        allOrders.push(res)
+      } catch (error) {
+        throw new Error(error)
+      }
+    }
+  }
+  return allOrders
+}
 
-waiter();
+const indicationsWaiter1 = [
+  {
+    food: ['hamburger'],
+    table: 3
+  }
+]
+
+const indicationsWaiter2 = [
+  {
+    food: ['hotdog'],
+    table: 0
+  },
+  {
+    food: ['pizza'],
+    table: 2
+  }
+]
+
+const indicationsWaiter3 = [
+  {
+    food: ['hotdog', 'pizza', 'hotdog'],
+    table: 1
+  }
+]
+
+
+const getOrders = async () => {
+  waiters(indicationsWaiter1).then(res => console.log(`Waiter 1 says: ${res}`))
+
+  waiters(indicationsWaiter2).then(res => console.log(`Waiter 2 says: ${res}`))
+
+  try {
+    const thirdWaiter = await waiters(indicationsWaiter3)
+    console.log(`Waiter 3 says: ${thirdWaiter}`)
+  } catch (error) {
+    throw error
+  }
+}
+
+const fetchOrders = () => {
+
+}
+
+
+getOrders()
