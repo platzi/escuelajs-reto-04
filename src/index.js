cdfe426212ece5474a9445ec5e2dc444358754af
wilantury <ventas@antury.net>
2019-09-21 11:17:19 -0500

    Solución a problemas 1, 2 y 3: Done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,46 @@
+const ramdomTime = ()=>{
+  const time = 1000 + Math.random()*7000;
+  return Math.floor(time)
+}
+
+const waiter2 = ()=>{
+  orders(ramdomTime(), menu.hotdog, table[0])
+    .then((res) => {
+      console.log(res)
+      return orders(ramdomTime(), menu.pizza, table[2])
+    })
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+}
+
+async function waiter3 () {
+  try{
+    const pedido1 = await orders(ramdomTime(), menu.hotdog, table[1])
+    //console.log(pedido1)
+    const pedido2 = await orders(ramdomTime(), menu.pizza, table[1])
+    //console.log(pedido2)
+    const pedido3 = await orders(ramdomTime(), menu.hotdog, table[1])
+    //console.log(pedido3)
+    console.log(`Platillos de la mesa ${table[1]} servidos`)
+  }catch(error){
+    console.log(`ha ocurrido el siguiente error: ${error}`)
+  }   
+
+}
+
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
+    if(!product && !table){
+      reject(`Orden llegó sin producto ni mesa`)
+    }else if(!product){
+      reject(`Orden llegó sin producto`)
+    }
+    else if(!table){
+      reject(`Orden llegó sin número de mesa`)
+    }
     setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
+      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);      
     }, time);
   });
 }
@@ -16,9 +54,11 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(ramdomTime(), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
 
-waiter();
+//waiter();
+//waiter2();
+waiter3();
