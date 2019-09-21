9da0c0e2ff773129289f4dcb07c32593b6509a3c
Dumar Ojeda <dumarojeda@gmail.com>
2019-09-21 11:16:45 -0500

    Resolviendo el primer problema

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-const orders = (time, product, table) => {
+const orders = (time, product, table, state) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
-    }, time);
+    if (state) {
+      setTimeout(() => {
+        resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
+      }, time);
+    } else {
+      setTimeout(() => {
+        reject(`=== Lo sentimos pero el Pedido ${product}, no se va poder servir en la ${table} porque se demoro ${time}ms`)
+      }, time)
+    }
   });
 }
+
+const randomState = () => Math.floor(Math.random() * Math.floor(2))
+
+const randomTime = () => Math.floor(Math.random() * (1500 - 1000) + 1000)
 
 const menu = {
   hamburger: 'Combo Hamburguesa',
@@ -16,7 +26,7 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(randomTime(), menu.hamburger, table[3], randomState())
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
