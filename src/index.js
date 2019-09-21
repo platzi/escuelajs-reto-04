635eb58a68062d53bc3dc553a2b4652fb59db21e
cesar galindo <csgalindos@hotmail.com>
2019-09-21 11:19:58 -0500

    resuelto el primer punto

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ const orders = (time, product, table) => {
     setTimeout(() => {
       resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
     }, time);
-  });
+      reject (`== el pedido no ha sido servido y esta siendo preparado ==`)
+  }
+  )
+}
+
+function randomTime() {
+    let max = 8
+    let min = 1
+      let tiempoRandom = (Math.floor(Math.random()*(max-min+1)+min))*1000
+      return tiempoRandom
 }
 
 const menu = {
@@ -15,10 +24,27 @@ const menu = {
 
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
-const waiter = () => {
+const waiter = (table) => {
   orders(6000, menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
 
-waiter();
+function wainter2() {
+  let nuevosclientes = 1
+  return new Promise((resolve,reject)=>{
+    if (nuevosclientes>0) {
+      resolve ('tomaPedido')
+    } reject ('= esperar por el pedido ==')
+  })
+}
+
+tomaPedido ()
+  .then (()=>{
+
+  })
+
+// waiter();
+
+
+console.log(randomTime())
