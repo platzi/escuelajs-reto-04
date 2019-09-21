b75e4b13f8c7cee00fb8f7a67cbced8a7d9d8991
Rodrigo Ortiz <rodrigortizm@gmail.com>
2019-09-21 10:56:54 -0500

    Solucion Segundo Problema

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const orders = (time, product, table) => {
   return new Promise((resolve, reject) => {
 
     setTimeout(() => {
-      if (false) {
+      if (true) {
         resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);  
       } else {
         reject(`!== Pedido falló: ${product}, tiempo de preparación ${time}ms para la ${table}`);
@@ -30,4 +30,19 @@ const waiter = () => {
     .catch((err) => console.error(err));
 };
 
+// orders(randomTime(), menu.hotdog, table[0]);
+// orders(randomTime(), menu.pizza, table[2]);
+
+const waiter2 = () => {
+  orders(randomTime(), menu.hotdog, table[0])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+  orders(randomTime(), menu.pizza, table[2])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+};
+
+
 waiter();
+waiter2();
+
