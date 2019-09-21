26de9a913dec17df76492a3f17c432fd27fe787e
Julian Toro <jtoro@tecmovin.com>
2019-09-21 10:37:42 -0500

    Retor punto 1 punto 2 punto 3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
-    }, time);
+      if (!product) 
+        reject("no hay producto")
+      setTimeout(() => {
+        resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
+      }, time);
   });
 }
+
+const MAX = 1000
+const MIN = 8000
+const randomTime = () => Math.floor(Math.random() * (MAX - MIN + 1) + MIN); 
 
 const menu = {
   hamburger: 'Combo Hamburguesa',
@@ -16,9 +22,37 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(randomTime(), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
 
 waiter();
+
+const waiter2 = () => {
+  orders(randomTime(), menu.hotdog, table[0])
+    .then((res) => {
+      console.log(res) 
+      return orders(randomTime(), menu.pizza, table[2])
+    })
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err))
+}
+
+waiter2()
+
+const waiter3 = async function (){
+  try{
+    const ordenes = [
+      orders(randomTime(), menu.hotdog, table[1]),
+      orders(randomTime(), menu.pizza, table[1]),
+      orders(randomTime(), menu.hotdog, table[1])
+    ]
+    const res = await Promise.all(ordenes)
+    console.log(res)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+waiter3()
