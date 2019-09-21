8f41734364e95fd0c998cec0838adf2bf418ae94
Omar Flores Grimontt <omar.flores@3dves.com>
2019-09-21 18:58:56 -0500

    added axios for request, resolved all challenges

--- src/index.js
+++ src/index.js
@@ -1,41 +1,8 @@
-const randomTime = () => Math.round(Math.random() * (8000 - 1000) + 1000)
+const axios = require('axios')
 
-const errorHandler = (time, product, table) => {
-  if (!time || !typeof time === 'int') {
-    return 'No tenemos un tiempo para su pedido'
-  }
+const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/orders'
 
-  if (!table || !tables.includes(table)) {
-    if (table) {
-      return `Lo sentimos, no tenemos la mesa ${table} disponible`
-    }
-    return 'Lo sentimos, no podemos atenderle si no nos pide una mesa'
-  }
-
-  if (!product || !Object.keys(menu).includes(product)) {
-    if (product) {
-      return `El producto ${product} no se encuentra disponible`
-    }
-    return 'Lo sentimos, no podemos servirle si no especifica su pedido'
-  }
-
-  return false
-}
-
-
-const orders = (time, product, table) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const error = errorHandler(time, product, table)
-
-      if (error) {
-        reject(error)
-      }
-
-      resolve(`Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table} \n`)
-    }, time)
-  })
-}
+const finishedWaiters = []
 
 const menu = {
   hamburger: 'Combo Hamburguesa',
@@ -44,24 +11,6 @@ const menu = {
 }
 
 const tables = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5']
-
-const waiters = async (indications) => {
-  const time = randomTime()
-  const allOrders = []
-  let res
-  for (indication of indications) {
-    const { food, table } = indication
-    for (order of food) {
-      try {
-        res = await orders(time, order, tables[table])
-        allOrders.push(res)
-      } catch (error) {
-        throw new Error(error)
-      }
-    }
-  }
-  return allOrders
-}
 
 const indicationsWaiter1 = [
   {
@@ -88,23 +37,108 @@ const indicationsWaiter3 = [
   }
 ]
 
+const randomTime = () => Math.round(Math.random() * (8000 - 1000) + 1000)
 
-const getOrders = async () => {
-  waiters(indicationsWaiter1).then(res => console.log(`Waiter 1 says: ${res}`))
+const errorHandler = (time, product, table) => {
+  if (!time || !typeof time === 'int') {
+    return '😞: Lo sentimos, no tenemos un tiempo para su pedido'
+  }
 
-  waiters(indicationsWaiter2).then(res => console.log(`Waiter 2 says: ${res}`))
+  if (!table || !tables.includes(table)) {
+    if (table) {
+      return `😞: Lo sentimos, no tenemos la mesa ${table} disponible`
+    }
+    return '😞: Lo sentimos, no podemos atenderle si no nos pide una mesa'
+  }
 
+  if (!product || !Object.keys(menu).includes(product)) {
+    if (product) {
+      return `😞: Lo sentimos, no tenemos ${product}`
+    }
+    return '😞: Lo sentimos, no podemos servirle si no especifica su pedido'
+  }
+
+  return null
+}
+
+const requestFood = (time, product, table) => {
+  console.log(`📣: Recibida orden de ${product} para ${table}`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const error = errorHandler(time, product, table)
+      error && reject(error)
+      resolve(`🤖: Pedido servido de ${product} a la ${table}. Tiempo de preparación de ${time}ms`)
+    }, time)
+  })
+}
+
+const serveTable = (indications) => {
+  const time = randomTime()
+  const allOrders = []
+
+  indications.forEach(indication => {
+    const { food, table } = indication
+    food.forEach(order => allOrders.push(requestFood(time, order, tables[table])))
+  })
+
+  return Promise.all(allOrders)
+}
+
+const attendVipClients = () => {
+  const getVipSuccessAnswer = (res, waiter) => {
+    res.forEach(phrase => console.log(`Mesonero ${waiter} ${phrase}`))
+    finishedWaiters.push(waiter)
+  }
+
+  const getVipErrorAnswer = (err, waiter) => {
+    finishedWaiters.push(waiter)
+    console.log(`Mesonero ${waiter} ${err}`)
+  }
+
+  serveTable(indicationsWaiter1)
+    .then(res => getVipSuccessAnswer(res, 1))
+    .catch(err => getVipErrorAnswer(err, 1))
+
+  serveTable(indicationsWaiter2)
+    .then(res => getVipSuccessAnswer(res, 2))
+    .catch(err => getVipErrorAnswer(err, 2))
+}
+
+const attendRegularClients = async () => {
   try {
-    const thirdWaiter = await waiters(indicationsWaiter3)
-    console.log(`Waiter 3 says: ${thirdWaiter}`)
+    const res = await serveTable(indicationsWaiter3)
+    finishNonPrioritizedAttendance(res, 2, 3)
   } catch (error) {
-    throw error
+    finishNonPrioritizedAttendance(error, 2, 3)
   }
 }
 
-const fetchOrders = () => {
+const finishNonPrioritizedAttendance = (message, priority, waiter) => {
+  if (finishedWaiters.length === priority) {
+    if (typeof message === 'object') {
+      message.forEach(phrase => console.log(`Mesonero ${waiter} ${phrase}`))
+    } else {
+      console.log(`Mesonero ${waiter} ${message}`)
+    }
+    finishedWaiters.push(waiter)
+  } else {
+    setTimeout(() => finishNonPrioritizedAttendance(message, priority, waiter), 0)
+  }
+}
 
+const fetchOrders = async () => {
+  try {
+    const res = await axios.get(API)
+    console.log(res.data)
+    finishNonPrioritizedAttendance(res.data, 3, 4)
+  } catch (error) {
+    finishNonPrioritizedAttendance(`😞: ${error.message}`, 3, 4)
+  }
 }
 
 
-getOrders()
+fetchOrders()
+
+attendRegularClients()
+
+attendVipClients()
