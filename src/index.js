113b1fbad147264ea938e988077e27270afe1cc0
William Velazquez <34907456+WilliamVelazquez@users.noreply.github.com>
2019-09-21 11:18:05 -0500

    Solving 1st Problem
    Creating randomTime Function that returns a number between 1000 and 8000.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+const randomTime = () => {
+  return Math.floor((Math.random() * 7000) + 1000);
+};
+
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
@@ -16,7 +20,7 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(randomTime(), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
