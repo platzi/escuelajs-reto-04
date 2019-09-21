aabcd9b26837ff48d213fddb4930acbc50b81089
Jesús Antonio García Valadez <jesus.garciav@me.com>
2019-09-21 10:38:53 -0500

    feat: add randomTime function
    handle reject cases when product or table is not defined.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+const randomTime = (min = 1000, max = 8000) =>
+  Math.floor(Math.random() * (max - min) + min);
+
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
+    if (!time) {
+      reject('No tenemos un tiempo definido para entregar el pedido.')
+    }
+
+    if (!product) {
+      reject('No se puede servir el pedido. No nos han dicho qué van a pedir.')
+    }
+
+    if (!table) {
+      reject('No se puede servir el pedido. No sabemos a que mesa llevarlo.')
+    }
+
     setTimeout(() => {
       resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
     }, time);
@@ -16,7 +31,7 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(randomTime(), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
