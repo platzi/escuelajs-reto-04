0c98db40d4768c283dc31f37d055a8e410218cc1
Fabio Cortes <fcor14@hotmail.com>
2019-09-21 10:52:32 -0500

    Reto 1: Done - Agregué una pequeña condición para rechazar la orden

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+const MAX_TIME = 10000;
+const MIN_TIME = 1000;
+const MAX_EXPECTED_TIME = 8000;
+
+const randomTime = () => Math.floor(Math.random() * ((MAX_TIME + 1) - MIN_TIME) + MIN_TIME);
+
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
+      if (time > MAX_EXPECTED_TIME ) {
+        reject(`=== El pedido: ${product} tardó mucho en salir, se demoró ${time}ms, el cliente ya se fue :(`)
+      }
       resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
     }, time);
   });
@@ -16,7 +25,7 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(randomTime(), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
