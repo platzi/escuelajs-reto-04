04c96c9b9cd0fe9b6944455d081ed517742ba93d
Miguel Hernandez <miguel@DESKTOP-FKGSUGO.localdomain>
2019-09-21 11:14:33 -0500

    Reto 1 - terminado

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
+    if (!time || !product || !table || time > 8000) {
+      reject(
+        `=== Error al realizar el pedido tiempo: ${time} producto: ${product} mesa: ${table}`
+      );
+    }
     setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
+      resolve(
+        `=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`
+      );
     }, time);
   });
-}
-
-const menu = {
-  hamburger: 'Combo Hamburguesa',
-  hotdog: 'Combo Hot Dogs',
-  pizza: 'Combo Pizza',
 };
 
-const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
+const menu = {
+  hamburger: "Combo Hamburguesa",
+  hotdog: "Combo Hot Dogs",
+  pizza: "Combo Pizza"
+};
+
+const table = ["Mesa 1", "Mesa 2", "Mesa 3", "Mesa 4", "Mesa 5"];
+
+function randomTime() {
+  return Math.floor(Math.random() * 8000 + 1000);
+}
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+  orders(randomTime(), menu.hamburger, table[3])
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
 };
 
 waiter();
