180bb66c95eebe56320a0a0c852d8a59b2748bcf
Jefferson Aguilar <jeffersonaguilar@outlook.com>
2019-09-21 10:27:00 -0500

    Reto 1, Reto 2, Reto3

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ const menu = {
   pizza: 'Combo Pizza',
 };
 
+const randomTime = () => {
+  return Math.floor(Math.random() * ((8000+1)-1000) + 1000);
+}
+
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
@@ -21,4 +25,30 @@ const waiter = () => {
     .catch((err) => console.error(err));
 };
 
+const waiter2 = () => {
+  orders(randomTime(), menu.pizza, table[0])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+
+    orders(randomTime(), menu.hotdog, table[2])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+};
+
+const waiter3 = async () => {
+  const orderCustomer1 = orders(randomTime(), menu.pizza, table[1])
+  const orderCustomer2 = orders(randomTime(), menu.hamburger, table[1])
+  const orderCustomer3 = orders(randomTime(), menu.hotdog, table[1])
+
+  const processOrders = await Promise.all([orderCustomer1, orderCustomer2, orderCustomer3])
+  processOrders.forEach((orders) => {
+    console.log(orders);
+  })
+};
+
+
+
+
 waiter();
+waiter2();
+waiter3();
