9f09bad849db14459f4f2e57cdc668b843ceb243
droogadulce <ahigareda.mayra@gmail.com>
2019-09-21 11:44:06 -0500

    Correcciones: cambio a arrow function

--- src/index.js
+++ src/index.js
@@ -35,11 +35,11 @@ const waiter = () => {
  * Retorna un valor en mili segundos de forma aleatoria en el rango de
  * 1000ms hasta 8000ms.
  */
-function randomTime() {
-  const MAX = 9000; // excluded
+const randomTime = () => {
+  const MAX = 8000; // excluded
   const MIN = 1000;
   return Math.floor(Math.random() * (MAX - MIN)) + MIN;
-}
+};
 
 const waiter2 = () => {
   orders(randomTime(), menu.hotdog, table[0])
@@ -53,10 +53,10 @@ const waiter2 = () => {
 
 const waiter3 = async () => {
   try {
-    let pedido1 = await orders(randomTime(), table[1]);
+    let pedido1 = await orders(randomTime(), menu.hotdog, table[1]);
     let pedido2 = await orders(randomTime(), menu.pizza, table[1]);
     let pedido3 = await orders(randomTime(), menu.hotdog, table[1]);
-    console.log('Ordenes listas');
+    console.log('Órdenes listas');
     console.log(pedido1);
     console.log(pedido2);
     console.log(pedido3);
@@ -66,8 +66,8 @@ const waiter3 = async () => {
 };
 
 // Primer problema
-//waiter();
+waiter();
 // Segundo problema
-//waiter2();
+waiter2();
 // Tercer problema
 waiter3();
