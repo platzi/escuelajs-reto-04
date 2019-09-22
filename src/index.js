cd60ba5ebf9d695beb5b110f758930553b3c4d53
cristubuntu <cristhiam.tovar@mail.escuelaing.edu.co>
2019-09-21 23:37:44 -0500

    waiter2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const orders = (time, product, table) => {
-  console.log(`### Orden: ${product} para ${table}`);
+  
+  console.log(`### Orden: ${product} para la ${table}`);
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
-    }, time);
+      setTimeout(() => {
+        resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
+      }, time);     
   });
 }
 
@@ -13,12 +14,71 @@ const menu = {
   pizza: 'Combo Pizza',
 };
 
+//                0         1         2         3         4
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
+function randomTime (){ 
+  const min=1000; 
+  const max=8000;  
+  let time = Math.random() * (+max - +min) + +min; 
+  return time.toFixed(0);
+}
+
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(randomTime(), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
+// Pedido "Mesa 1": Combo Hotdog Pedido "Mesa 3": Combo Pizza
+async function waiter2(tableId, menuId){
 
+  return new Promise ((resolve,reject)=>{
+    
+      await resolve( orders(randomTime(), menu[menuId], table[tableId]));
+      
+    
+      // reject(`Veci se nos acabaron los ${menu[menuId]}`)
+    
+
+  })
+}
+waiter2(0,'hotdog')
+    .then( resolve => {
+      console.log(resolve)
+      return waiter2(0,'pizza')
+    })
+    .then( resolve => {
+      console.log(resolve)
+    })
+    .catch((err) => console.error(err));
+
+// Pedido "Mesa 2": Combo Hotdog, Combo Pizza, Combo Hotdog
+
+// Utiliza Async/Await
+
+// Manejo de errores
+
+// Utiliza la función de randomTime
+const waiter3 = () => {
+  ordersMesa2 = async () => {
+    const combo1 = await orders(randomTime(),menu.hotdog, table[3]);
+    const combo2 = await orders(randomTime(),menu.pizza, table[3]);
+    const combo3 = await orders(randomTime(),menu.hotdog, table[3]);
+    Promise.all([combo1,combo2,combo3])
+    return combo1, combo2, combo3; 
+  }
+
+  orders(randomTime(), menu.hamburger, table[3])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+ 
+return ordersMesa2;
+  };
+console.log('mesero default');  
 waiter();
+console.log('mesero 2');
+waiter2();
+console.log('mesero 3');
+waiter3();
+console.log('  ');
+console.log('  ');
