77720ac711688ad903e3930b4b6f9fbcad56db46
Ruber Hernandez <ruberhenandez@gmail.com>
2019-09-22 12:52:58 -0500

    se hace reto1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const orders = (time, product, table) => {
     setTimeout(() => {
       resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
     }, time);
+    if (!product) {
+      reject(`No se tiene productos`)
+    }
   });
 }
 
@@ -15,10 +18,23 @@ const menu = {
 
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
+
+// Primer problema
+// Crea una función llamada "randomTime" que te permita retornar un valor en mili segundos de forma aleatoria en el rango de 1000ms hasta 8000ms.
+// Completa la función "orders" manejando el reject.
+// Utiliza la función de randomTime
+function randomTime() {
+  var min = 1000, max = 8000;
+  var rand = Math.floor((Math.random() * (max - min + 1)) + min);
+  return rand;
+}
+
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
-    .then((res) => console.log(res))
+  orders(randomTime(), menu.hotdog, table[3])
+    .then((respuesta) => console.log(respuesta))
     .catch((err) => console.error(err));
 };
 
+
 waiter();
+
