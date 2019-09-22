12f5ebce64883a3d7f2d244f30eb4161771a8879
luisrdz5 <luis@luisrodriguez.com>
2019-09-21 22:56:15 -0500

    Se ha resuelto el primer problema del reto

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+const randomTime = (min, max) => (Math.floor(Math.random()* (max - min) + min));
+
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
     }, time);
+    if(!time || !product || !table){
+      reject(new Error(' La orden no ha podido ser creada por que no fue recibido un requerimento '));
+    }
+    
   });
 }
 
@@ -16,7 +22,7 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(randomTime(1000,8000), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
