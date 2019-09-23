6db9ffdf92791486f80fd6af72b86fe43e87e338
amorales <blue_bufi@hotmail.com>
2019-09-23 01:17:02 -0500

    Add challenge 4

--- src/index.js
+++ src/index.js
@@ -20,7 +20,8 @@ const menu = {
 
 const constants = { 
     MIN_VALUE : 1000, 
-    MAX_VALUE : 8000 
+    MAX_VALUE : 8000,
+    API_URL: 'https://us-central1-escuelajs-api.cloudfunctions.net/orders'
 };
 
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
@@ -33,29 +34,58 @@ const randomTime = () => {
 const waiter = () => {
   orders(randomTime(), menu.hamburger, table[3])
     .then((res) => console.log(res))
-    .catch((err) => console.error(`Ocurrio un error con el mesero 1: ${err}`));
+    .catch((err) => console.error(`Ocurrió un problema con el mesero 1: ${err}`));
 };
 
 const waiter2 = () => {
-  orders(randomTime(), menu.pizza, table[1])
+  orders(randomTime(), menu.hotdog, table[0])
     .then((res) => {
       console.log(res)
-      return orders(randomTime(), menu.hotdog, table[3])
+      return orders(randomTime(), menu.pizza, table[2])
     })
     .then((res) => console.log(res))
-    .catch((err) => console.error(`Ocurrio un error con el mesero 2: ${err}`))
+    .catch((err) => console.error(`Ocurrió un problema con el mesero 2: ${err}`))
 };
 
 const waiter3 = async() => {
-  const table2Order = [orders(randomTime(), menu.pizza, table[2]),  orders(randomTime(), menu.hotdog, table[2]),  orders(randomTime(), menu.hamburger, table[2])]
+  const table2Order = [orders(randomTime(), menu.hotdog, table[1]),  orders(randomTime(), menu.pizza, table[1]),  orders(randomTime(), menu.hotdog, table[1])]
    try {
       const orderForTable2 = await Promise.all(table2Order)
       console.log(orderForTable2)
    } catch (error) {
-      console.error(`Ocurrio un error con el mesero 3: ${error}`)
+      console.error(`Ocurrió un problema con el mesero 3: ${error}`)
    }
 };
 
-waiter();
-waiter2();
-waiter3();
+const fetch = require('node-fetch');
+
+const fetchOrders = () => {
+  fetch(constants.API_URL)
+  .then(res => res.json())
+  .then(json => {
+    console.log(`Pedido recibido desde la API: ${json.data}`)
+    return json.data
+  })
+  .catch(error => console.error(`Algo salio mal :( : ${error}`))
+}
+
+
+
+const waiter4 = async() => {
+  const dd = [fetchOrders(), fetchOrders(), fetchOrders(), fetchOrders()]
+  const APIOrder = [orders(randomTime(), fetchOrders(), table[1]),  orders(randomTime(), fetchOrders(), table[1]), orders(randomTime(), fetchOrders(), table[1]), orders(randomTime(), fetchOrders(), table[1])]
+  console.log(APIOrder);
+   try {
+      const orderFromAPI = await Promise.all(APIOrder)
+      console.log(orderFromAPI)
+   } catch (error) {
+      console.error(`Ocurrio un error con el mesero 4: ${error}`)
+   }
+};
+
+
+// waiter();
+// waiter2();
+// waiter3();
+//fetchOrders();
+waiter4();
