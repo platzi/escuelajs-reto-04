25e51beb7f25d4ff627cbfdade1900d4d47d49c5
William Velazquez <34907456+WilliamVelazquez@users.noreply.github.com>
2019-09-23 02:57:17 -0500

    Solving 4th Problem
    Adding node-fetch dependency.
    Updating randomTime function with Max and Min Time.
    Creating fetchOrders function.
    Updating Console Errors.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
+//@Author William E. Velázquez A. - info@williamvelazquez.com
+const fetch = require('node-fetch');
+
+const API_BASE ='https://us-central1-escuelajs-api.cloudfunctions.net';
+const API_URL =`${API_BASE}/orders`;
+
+const MIN_TIME = 1000;
+const MAX_TIME = 8000;
+
+const fetchOrders = () => {
+  return fetch(API_URL).then( res => res.json());
+};
+
 const randomTime = () => {
-  return Math.floor((Math.random() * 7000) + 1000);
+  return Math.floor((Math.random() * (MAX_TIME - MIN_TIME)) + MIN_TIME);
 };
 
 const orders = (time, product, table) => {
-  return new Promise((resolve, reject) => {
+  return new Promise( (resolve, reject) => {
     if (time && product && table) {
       console.log(`### Orden: ${product} para ${table}`);
-      setTimeout(() => {
+      setTimeout( () => {
         resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
       }, time);
     } else {
       reject('Favor de brindar los datos necesarios para solicitar la orden')
     }
   });
-}
+};
 
 const menu = {
   hamburger: 'Combo Hamburguesa',
@@ -25,19 +38,19 @@ const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
   orders(randomTime(), menu.hamburger, table[3])
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+    .then( (res) => console.log(res))
+    .catch( (error) => console.error(`No se pudo realizar la orden para la ${table[3]} --- ERROR: ${error}`));
 };
 
 const waiter2 = () => {
   orders(randomTime(), menu.hotdog, table[0])
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+    .then( (res) => console.log(res))
+    .catch( (error) => console.error(`No se pudo realizar la orden para la ${table[0]} --- ERROR: ${error}`));
   
   orders(randomTime(), menu.pizza, table[2])
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
-}
+    .then( (res) => console.log(res))
+    .catch( (error) => console.error(`No se pudo realizar la orden para la ${table[2]} --- ERROR: ${error}`));
+};
 
 const waiter3 = async () => {
   try {
@@ -50,10 +63,29 @@ const waiter3 = async () => {
       ${dish3}
     `);
   } catch (error) {
-    console.log(`ERROR: ${error}`)
+    console.error(`No se pudo realizar la orden para la ${table[1]} --- ERROR: ${error}`);
   } 
+};
+
+const waiter4 = async () => {
+  try {
+    console.log(`### Solicitando orden para ${table[4]}`);
+    const dish1 = await fetchOrders();
+    const dish2 = await fetchOrders();
+    const dish3 = await fetchOrders();
+    const dish4 = await fetchOrders();
+    console.log(`Se sirve en ${table[4]}:
+      === Pedido servido: ${dish1.data}
+      === Pedido servido: ${dish2.data}
+      === Pedido servido: ${dish3.data}
+      === Pedido servido: ${dish4.data}
+    `);
+  } catch (error) {
+    console.error(`No se pudo realizar la orden para la ${table[4]} --- ERROR: ${error}`);
+  }
 }
 
 waiter();
 waiter2();
 waiter3();
+waiter4();
