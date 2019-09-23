8374065ce362784445f046a0bde9fca6c9f6daff
sue <sue@MacBook-Pro-de-sue.local>
2019-09-23 12:12:30 -0500

    adding new fetch function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const orders = (time, product, table) => {
-  console.log(`### Orden: ${product} para ${table}`);
+  console.log(` camara preparame una ### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
@@ -21,10 +21,9 @@ const waiter = () => {
     .catch((err) => console.error(err));
 };
 
-//waiter();
+// waiter();
 
 // 1st CHALLENGE:
-// create a function that gets random numbers between 1 yo 8
 
 function randomTime() {
  let randomNumber = Math.round(Math.random() * (8000 -  1000) + 1000);
@@ -33,6 +32,7 @@ function randomTime() {
 randomTime();
 
 // 2nd CHALLENGE:
+
 const waiter2 = () => {
   orders(randomTime(),menu.hamburger, table[0])
     .then((res) => console.log(res))
@@ -40,7 +40,42 @@ const waiter2 = () => {
       return orders(randomTime(),menu.pizza, table[2])
       .then((res) => console.log(res))
     })
-    .catch((err) => console.error(err));
-    
+    .catch((err) => console.error(err));    
 }
-waiter2();
+ // waiter2();
+
+// 3nd CHALLENGE:
+
+const clientOne = orders(randomTime(),menu.hotdog, table[1]);
+const clientTwo = orders(randomTime(),menu.pizza, table[1]);
+const clientThree = orders(randomTime(),menu.hotdog, table[1]);
+
+const threeOrders = [clientOne,clientTwo,clientThree]
+
+async function waiter3() {
+  try {
+    await Promise.all(threeOrders)
+    .then(
+      result => setTimeout(()=>{
+        result.map(order => console.log(`ROBOTCITO MESERO 3 DICE: ¡bon appetit! ${order}`))
+      },1000)
+    )
+  }
+  catch { error => console.log( `buuu esto se murio :( ${error}`)
+  }
+}
+// aqui aplique el settimeout por que dice el reto pero no entiendo para que, no se si estoy haciendo mal mi codigo por que se tarda en resolver las promesas,
+// y siento que ponerle el settime no lo sigue, o ese tiempo que estoy esperando es el tiempo de la petición o del set :s 
+
+ waiter3();
+
+// 4rd OPTIONAL CHALLENGE:
+
+async function fetchOrders(){
+  const url = ' https://us-central1-escuelajs-api.cloudfunctions.net/orders';
+  const result = await fetch(url)
+  const data = await result.json()
+  console.log(data)
+}
+fetchOrders()
+
