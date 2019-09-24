7a2c029100262d1817775f3b82022d0dc3c24c5e
sebastian07leal <sebastianleal2016@gmail.com>
2019-09-24 13:21:43 -0500

    Reto tres solucionado, se realizo el metodo de fetch, falto el metoso waiter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
+
 const menu = {
-hamburger: 'Combo Hamburguesa',
+  hamburger: 'Combo Hamburguesa',
   hotdog: 'Combo Hot Dogs',
   pizza: 'Combo Pizza',
 };
 
 const pedidoWaiter = {
-  Combo: 'Combo Hamburguesa', 
+  Combo: 'Combo Hamburguesa',
   table: 3
 }
 
@@ -27,60 +28,61 @@ const pedidoWaiter3 = {
   table: 1
 }
 
-const randomTime = () => Math.floor( Math.random() * (8000 - 1000 ) + 1000 ); 
+
+const randomTime = () => Math.floor(Math.random() * (8000 - 1000) + 1000);
 
 
-const controllerErrors = (timeOrder, orderEat, tableOrder) => {  
+const controllerErrors = (timeOrder, orderEat, tableOrder) => {
 
   if (!timeOrder) {
-    
-    return  'Error no se encontro el tiempo de la peticion';
-    
-  } else if (!orderEat ) {
-    
+
+    return 'Error no se encontro el tiempo de la peticion';
+
+  } else if (!orderEat) {
+
     return 'Error no se encontro la orden';
 
   } else {
-    
+
     if (!tableOrder && tableOrder != 0) {
-   
-      return `Error, no se asigno ninguna mesa: ${tableOrder}`; 
-       
+
+      return `Error, no se asigno ninguna mesa: ${tableOrder}`;
+
     }
-    
+
   }
-  
+
 }
 
-const orders = (time, product, tableNum) => {  
+const orders = (time, product, tableNum) => {
 
-  
+
 
   return new Promise((resolve, reject) => {
 
     const error = controllerErrors(time, product, tableNum);
-    
+
     if (error) {
 
       reject(error);
 
-    }                    
-    
+    }
+
     console.log(`### Orden: ${product} para ${table[tableNum]}`);
-    
+
     setTimeout(() => {
       resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table[tableNum]}`);
     }, time);
-    
+
   });
 }
 
-const waiter = (ordenEnviada) => {  
+const waiter = (ordenEnviada) => {
 
   console.log('---PEDIDO WAITER---');
-  
 
-  orders(randomTime(), ordenEnviada.Combo, ordenEnviada.table)  
+
+  orders(randomTime(), ordenEnviada.Combo, ordenEnviada.table)
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 
@@ -89,48 +91,67 @@ const waiter = (ordenEnviada) => {
 const waiter2 = (ordenEnviada = []) => {
 
   console.log('---PEDIDO WAITER2---');
-  
+
 
 
   orders(randomTime(), ordenEnviada[0].Combo, ordenEnviada[0].table)
-    .then( (dataPeticion) => {
+    .then((dataPeticion) => {
       console.log(dataPeticion);
       orders(randomTime(), ordenEnviada[1].Combo, ordenEnviada[1].table)
-      .then( (dataPeticion2) => {
-        console.log(dataPeticion2);
-      }) 
+        .then((dataPeticion2) => {
+          console.log(dataPeticion2);
+        })
     })
-    .catch( (err) => console.log(err))
-  
+    .catch((err) => console.log(err))
+
 }
 
 const waiter3 = async (ordenEnviada) => {
 
   console.log('---PEDIDO WAITER3---');
-  
-    
-      try {
 
-        const primerPLato = await orders(randomTime(), ordenEnviada.Combo[0], ordenEnviada.table); 
-        const segundoPlato = await orders(randomTime(), ordenEnviada.Combo[1], ordenEnviada.table);
-        const tercerPlato = await orders(randomTime(), ordenEnviada.Combo[2], ordenEnviada.table);
 
-        console.log(primerPLato);
-        console.log(segundoPlato);
-        console.log(tercerPlato);
-        
-      
-      } catch (err) {
+  try {
 
-        console.error(err);
-        
-      }
+    const primerPLato = await orders(randomTime(), ordenEnviada.Combo[0], ordenEnviada.table);
+    const segundoPlato = await orders(randomTime(), ordenEnviada.Combo[1], ordenEnviada.table);
+    const tercerPlato = await orders(randomTime(), ordenEnviada.Combo[2], ordenEnviada.table);
+
+    console.log(primerPLato);
+    console.log(segundoPlato);
+    console.log(tercerPlato);
+
+
+  } catch (err) {
+
+    console.error(err);
+
+  }
 
 
 }
 
-waiter(pedidoWaiter);
-waiter2(pedidoWaiter2); 
-waiter3(pedidoWaiter3); 
+const fetchOrders = async (pedidosMenu) => {
+
+  try {
+
+    const response = await fetch(pedidosMenu);
+    let data = await response.json();
+
+    return data;
+
+
+  } catch (err) {
+    console.error('Hubo un error: ' + err);
+    reject('Hubo un error: ' + err)
+  }
+
+}
+
+//fetchOrders(URL);
+
+// waiter(pedidoWaiter);
+// waiter2(pedidoWaiter2); 
+// waiter3(pedidoWaiter3); 
 
 
