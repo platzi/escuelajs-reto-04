cf4e207c9d5ca0fc82332f161cdab5bea4063811
FrcGustavo <hidalgo_fco520@outlook.com>
2019-09-23 20:01:18 -0500

    add function generateOrders: generate a array with all orders, add waiter 3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+const randomTime = () => {
+  const min = 1000;
+  const max = 8000;
+  const time = Math.floor(Math.random() * (max - min)) + min;
+  return time;
+};
+
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
@@ -7,38 +14,60 @@ const orders = (time, product, table) => {
   });
 }
 
+const generateOrders = (preOrders) => {
+  if(preOrders.length <= 0) return new Error('You orders can not procesed');
+  let waitOrders = [];
+  preOrders.forEach(order => {
+    waitOrders.push(orders(randomTime(), order.dish, order.table));
+  });
+  return waitOrders;
+};
+
 const menu = {
   hamburger: 'Combo Hamburguesa',
   hotdog: 'Combo Hot Dogs',
   pizza: 'Combo Pizza',
 };
-
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
+// Waithers
 const waiter = () => {
   orders(randomTime(), menu.hamburger, table[3])
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+    .then((res) => {
+      console.log(`Waiter 1: ${res}`)
+    })
+    .catch((err) => {
+      console.log(err.message)
+    });
 };
 
 const waiterTwo = () => {
   orders(randomTime(), menu.pizza, table[0])
-  .then((res => {
-    console.log(res);
-    return orders(randomTime(), menu.hotdog, table[2]);
-  }))
-  .then((res) => { console.log(res) })
-  .catch((err) => {
-    throw err;
-  })
+    .then((res => {
+      console.log(`Waiter 2: ${res}`);
+      return orders(randomTime(), menu.hotdog, table[2]);
+    }))
+    .then((res) => { console.log(`Waiter 2: ${res}`) })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 
-const randomTime = () => {
-  const min = 1000;
-  const max = 8000;
-  const time = Math.floor(Math.random() * (max - min)) + min;
-  return time;
-};
+const waiterThree = async () => {
+  const preOrders = [
+    { dish: menu.hotdog, table: table[1] },
+    { dish: menu.hamburger, table: table[1] },
+    { dish: menu.pizza, table: table[1] },
+  ];
+  try {
+    const waitOrders = generateOrders(preOrders);
+    const completeOrders = await Promise.all(waitOrders);
+    completeOrders.forEach(order => console.log(`Waiter 3: ${order}`));
+  } catch(err) {
+    console.log(err.message);
+  }
+}
 
 waiter();
 waiterTwo();
+waiterThree();
