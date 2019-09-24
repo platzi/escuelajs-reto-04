af15bc8e06146cc3b235bde7a9da95b10fdf50ca
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2019-09-24 18:29:13 -0500

    Reto 4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 // Reto 1:
 const randomTime = () => {
   const TIME_MIN = 1000;
@@ -17,6 +19,18 @@ const orders = (time, product, table) => {
     }
   });
 }
+// Reto 4:
+const fetchOrders = async (table) => {
+  const API_URL = 'https://us-central1-escuelajs-api.cloudfunctions.net/orders';
+  
+  console.log(`### Orden para ${table}`);
+  
+  return fetch(API_URL)
+    .then(response => response.json())
+    .then(data => {return `=== Pedido servido: ${data.data} para la ${table}`})
+    .catch(err => {return 'Error al acceder a la API'});
+}
+
 
 const menu = {
   hamburger: 'Combo Hamburguesa',
@@ -28,8 +42,8 @@ const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
   orders(randomTime(), menu.hamburger, table[3])
-    .then(res => console.log(res))
-    .catch(err => console.error(err));
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
 };
 // Reto 2:
 const waiter2 = () => {
@@ -55,10 +69,25 @@ const waiter3 = async () => {
   }
 }
 
+//Reto 4:
+const waiter4 = async () => {
+  let order1 = fetchOrders(table[4]);
+  let order2 = fetchOrders(table[4]);
+  let order3 = fetchOrders(table[4]);
+  let order4 = fetchOrders(table[4]);
 
+  try { 
+    const res = await Promise.all([order1, order2, order3,order4]);
+    res.map(any => console.log(any));
+  } catch(err){
+    console.log(err);
+  }
+}
 
 waiter();
 // Reto 2:
 waiter2();
 // Reto 3:
 waiter3()
+// Reto 4:
+waiter4()
