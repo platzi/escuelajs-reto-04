a9eff89886955c1a611e9f0780a1ad1eaa165b33
Luis Caraballo <luisred61@gmail.com>
2019-09-24 11:52:39 -0500

    Segundo reto resuelto

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ const maxTime = 8000;
 const minTime = 1000;
 
 const randomTime = () => {
-  var random =  Math.floor(Math.random() *(maxTime-minTime) + minTime )
+  var random = Math.floor(Math.random() * (maxTime - minTime) + minTime)
   return random;
 }
 
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
-  return new Promise( (resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    if (time && product && table) {
       setTimeout(() => {
         resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
       }, time);
+    } else {
+      reject((error) => (`Existe un problema en el pedido: ${error}`))
+    }
   });
 }
 
@@ -24,9 +28,20 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders( randomTime(), menu.hamburger, table[3])
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+  orders(randomTime(), menu.hamburger, table[3])
+    .then((response) => console.log(response))
+    .catch((error) => console.log(`Hubo un problema con el Mesero 1: ${error}`));
 };
 
+const waiter2 = () => {
+  orders(randomTime(), menu.hotdog, table[0])
+    .then((response) => {
+      console.log(response)
+      return orders(randomTime(), menu.pizza, table[2])
+    })
+    .then((response) => console.log(response))
+    .catch((error) => console.log(`Hubo un problema con el Mesero 2: ${error}`))
+}
+
 waiter();
+waiter2();
