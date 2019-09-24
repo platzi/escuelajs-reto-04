1f0496e4d45512f6dd5c9ad345aa09f9d8d3d9ce
cesar galindo <csgalindos@hotmail.com>
2019-09-23 20:49:02 -0500

    tercer punto

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-const orders = (time, product, table) => {
+function orders(time, product, table){
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
+    if(!product) {
+      reject (`=== El pedido no ha sido servido y esta siendo preparado ===`)
+    }
     setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
+      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table} ===`);
     }, time);
-      reject (`== el pedido no ha sido servido y esta siendo preparado ==`)
-  }
-  )
+  })
 }
 
 function randomTime() {
@@ -22,29 +23,58 @@ const menu = {
   pizza: 'Combo Pizza',
 };
 
-const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
+const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5']
 
 const waiter = (table) => {
   orders(6000, menu.hamburger, table[3])
     .then((res) => console.log(res))
-    .catch((err) => console.error(err));
-};
-
-function wainter2() {
-  let nuevosclientes = 1
-  return new Promise((resolve,reject)=>{
-    if (nuevosclientes>0) {
-      resolve ('tomaPedido')
-    } reject ('= esperar por el pedido ==')
-  })
+    .catch((err) => console.error(err))
 }
 
-tomaPedido ()
-  .then (()=>{
+const preparacion = {
+  hamburger: randomTime(),
+  hotdog: randomTime(),
+  pizza: randomTime ()
+}
 
-  })
+const wainter2 = () => {
+  orders(preparacion.hotdog,menu.hotdog,table[0])
+    .then((res) => {console.log(res)
+      return orders (preparacion.pizza, menu.pizza,table[2])
+    })
+    .then((res) => {console.log (res)})
+    .catch((err) => console.error(err))
+}
 
-// waiter();
+//wainter2()
 
+const waiter3__1 = () => {
+  orders(preparacion.hotdog,menu.hotdog,table[1])
+   .then((res) => console.log(res))
+   .catch((err) => console.log(err))
+}
 
-console.log(randomTime())
+const waiter3__2 = () => {
+  orders(preparacion.pizza,menu.pizza,table[1])
+   .then((res) => console.log(res))
+   .catch((err) => console.log(err))
+}
+
+const waiter3__3 = () => {
+  orders(preparacion.hotdog,menu.hotdog,table[1])
+   .then((res) => console.log(res))
+   .catch((err) => console.log(err))
+}
+
+const pedido = async() => {
+  try {
+    const comida1 = waiter3__1()
+    const comida2 = await waiter3__2(comida1)
+    const comida3 = await waiter3__3(comida2)
+      return await (comida3)
+  } catch (error) {
+    console.log((error) => console.log(error))
+  }
+}
+
+pedido().then(entrega => console.log(entrega))
