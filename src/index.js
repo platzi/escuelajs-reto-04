3464a341b66ba6e15bc9d29649f70b20cfd87c50
Eddy Giraldo <eddygiraldo@gmail.com>
2019-09-23 20:39:01 -0500

    finalización del reto 4

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+const fetch = require('node-fetch');
+
+const URL_ORDERS = 'https://us-central1-escuelajs-api.cloudfunctions.net/orders';
 const TIME_MAX = 8000;
 const TIME_MIN = 1000;
 
+//Generar tiempo aleatorio entre 1000 - 8000ms
 const randomTime = () => {
   return Math.floor(Math.random() * (TIME_MAX - TIME_MIN) + TIME_MIN);
 }
 
+//Envía a cocinar la orden
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
@@ -18,6 +23,18 @@ const orders = (time, product, table) => {
   });
 }
 
+//Petición a servicio externo por la orden
+const fetchOrders = async () => {
+  try {
+    const response = await fetch(URL_ORDERS);
+    const data = await response.json();
+    return data.data
+  } catch (error) {
+    return error
+  }
+
+}
+
 const menu = {
   hamburger: 'Combo Hamburguesa',
   hotdog: 'Combo Hot Dogs',
@@ -26,15 +43,18 @@ const menu = {
 
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
+//Genera una orden
 const waiter = () => {
   orders(randomTime(), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
 
+//Genera dos órdenes en cadena
+//Sirve una y cuando el EventLoop se desocupe sirve la otra D:
 const waiter2 = () => {
   orders(randomTime(), menu.hotdog, table[0])
-    .then((response) => {
+    .then(response => {
       console.log(response)
       return orders(randomTime(), menu.pizza, table[2])
     })
@@ -42,6 +62,7 @@ const waiter2 = () => {
     .catch((err) => console.error(err));
 }
 
+//Genera 3 órdenes y las sirve cuando todas estén listas
 const waiter3 = async () => {
   const orderPromises = [];
   orderPromises.push(orders(randomTime(), menu.hotdog, table[1]));
@@ -49,14 +70,35 @@ const waiter3 = async () => {
   orderPromises.push(orders(randomTime(), menu.hotdog, table[1]));
 
   try {
-    var response = await Promise.all(orderPromises);
-    console.log(response);    
+    let response = await Promise.all(orderPromises);
+    response.map(order => console.log(order));
   } catch (error) {
     console.log(error);
   }
+}
 
+//Obtiene la orden de un servicio externo,
+//Envia a la cocina cuando estén las 4 órdenes listas,
+//Sirve las órdenes cuando estén lista
+const waiter4 = async () => {
+  const fetchPromises = [];
+
+  for(let i = 0; i < 4; i++)
+  {
+    fetchPromises.push(fetchOrders());
+  }
+
+  try {
+    let response = await Promise.all(fetchPromises);
+    let promises = response.map(order => orders(randomTime(), order, table[4]));
+    let orderPromises = await Promise.all(promises);
+    orderPromises.map(order => console.log(order));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 waiter();
 waiter2();
 waiter3();
+waiter4();
