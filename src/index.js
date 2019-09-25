d128797382e9556f9840fff0572fce11e814a054
Jonnatan Merchan <Jonnatan.merchan@intcomext.com>
2019-09-25 17:16:29 -0500

    Solucion reto 4

--- src/index.js
+++ src/index.js
@@ -1,24 +1,89 @@
-const orders = (time, product, table) => {
-  console.log(`### Orden: ${product} para ${table}`);
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
-    }, time);
-  });
-}
+const fetch = require('node-fetch');
+const URL_ORDERS = 'https://us-central1-escuelajs-api.cloudfunctions.net/orders';
 
-const menu = {
-  hamburger: 'Combo Hamburguesa',
-  hotdog: 'Combo Hot Dogs',
-  pizza: 'Combo Pizza',
-};
+  const orders = (time, product, table) => {
+    console.log(`### Orden: ${product} para ${table}`);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
+      }, time);
+    });
+  }
+  const menu = {
+    hamburger: 'Combo Hamburguesa',
+    hotdog: 'Combo Hot Dogs',
+    pizza: 'Combo Pizza',
+  };
+  
+  const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
+  
+  const randomTime = (max = 8000, min = 1000) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+  
+  }
+  
+  const prepareOrder = (menuRest , tableRest) => {
+    orders(randomTime(), menuRest, tableRest)
+      .then((res) => console.log(res))
+      .catch((err) => console.error(err));
+  };
+  
+  const waiter =() => {
+    new Promise((resolve, reject) => {
+      resolve(prepareOrder( menu.hamburger, table[3]))
+    });
+  }
+  
+  const waiter2 = () => {
+    new Promise((resolve, reject) => {
+      resolve(prepareOrder( menu.hotdog, table[0]))
+    });
+    new Promise((resolve, reject) => {
+      resolve(prepareOrder( menu.pizza, table[2]))
+    });
+  };
+  
+  
+  
+  const waiter3 = async () => {
+    const orderlist = [
+     //primera orden 
+     prepareOrder(menu.pizza, table[1]),
+     //segunda orden
+     prepareOrder(menu.hamburger, table[1]),
+     //tercera orden
+     prepareOrder(menu.hotdog, table[1])
+    ]
+   
+     await Promise.all(orderlist).then(res => console.log(res))
+                                 .catch(err => console.log(err));;
+   };
 
-const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
+    //Consulta Api
+    const fetchOrders = async () => {
+        const responseOrder = await fetch(URL_ORDERS);
+        return await responseOrder.json();
+    }
 
-const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
-};
-
-waiter();
+  const waiter4 = async () => {
+    const fetchPromises = [];
+    //Creacion pedidos
+    for(let i = 0; i < 4; i++)
+    {
+      fetchPromises.push(fetchOrders());
+    }
+  
+    try {
+      let response = await Promise.all(fetchPromises);
+      let promises = response.map(order => orders(randomTime(), order, table[4]));
+      let orderPromises = await Promise.all(promises);
+      orderPromises.map(order => console.log(order));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  waiter();
+  waiter2();
+  waiter3();
+  waiter4();
