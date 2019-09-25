d6cc330cf0b1fa2b35517a5d09dd28eec2cc8eec
JuanAyal <juanayalasalazar1@gmail.com>
2019-09-24 19:40:09 -0500

    sulución reto 4

--- src/index.js
+++ src/index.js
@@ -2,23 +2,64 @@ const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
+      resolve(
+        `=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`
+      );
     }, time);
-  });
-}
 
-const menu = {
-  hamburger: 'Combo Hamburguesa',
-  hotdog: 'Combo Hot Dogs',
-  pizza: 'Combo Pizza',
+    // reject('Hubo un incendio en la cocina, tenemos que cerrar.')
+  });
 };
 
-const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
+const menu = {
+  hamburger: "Combo Hamburguesa",
+  hotdog: "Combo Hot Dogs",
+  pizza: "Combo Pizza"
+};
 
-const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+const table = ["Mesa 1", "Mesa 2", "Mesa 3", "Mesa 4", "Mesa 5"];
+
+const waiter = (menu, table) => {
+  prepareOrder(menu, table);
+};
+
+const prepareOrder = (menu, table) => {
+  orders(randomTime(), menu, table)
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+};
+
+const waiter2 = () => {
+  new Promise((resolve, reject) => {
+    resolve(prepareOrder(menu.hotdog, table[0]));
+  });
+
+  new Promise((resolve, reject) => {
+    resolve(prepareOrder(menu.pizza, table[2]));
+  });
+};
+
+const waiter3 = async () => {
+
+
+ const orderlist = [
+   //First order, table 2
+   prepareOrder(menu.pizza, table[1]),
+  //Second order, table 2
+  prepareOrder(menu.hamburger, table[1]),
+  //third order, table 2
+  prepareOrder(menu.hotdog, table[1])
+ ]
+
+  await Promise.all(orderlist).then(res => console.log(res))
+                                             .catch(err => console.log(err));;
+
+};
+
+const randomTime = (max = 8000, min = 1000) => {
+  return Math.floor(Math.random() * (max - min) + min);
 };
 
 waiter();
+waiter2();
+waiter3();
