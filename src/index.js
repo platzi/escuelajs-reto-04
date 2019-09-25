a5d5dcc34174abbc569e5db74a52e1fa2cf9a798
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2019-09-25 12:41:57 -0500

    First problem solved.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-const orders = (time, product, table) => {
-  console.log(`### Orden: ${product} para ${table}`);
+/* eslint-disable no-console */
+const randomTime = (min, max) => Math.ceil(Math.random() * (max - min) + min);
+
+const orders = (product, table) => {
+  console.log(`### Orden: ${product} para ${table}\n`);
   return new Promise((resolve, reject) => {
+    const time = randomTime(1000, 8200);
     setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
+      if (time < 8000) {
+        resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}\n`);
+      } else {
+        // simulate case reject when time is long in ms
+        reject(new TypeError(`Demora en la cocina (${time}ms).
+        -- No se pudó cumplir el pedido: ${product} de la ${table}`));
+      }
     }, time);
   });
-}
+};
 
 const menu = {
   hamburger: 'Combo Hamburguesa',
@@ -16,9 +26,10 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+  orders(menu.hamburger, table[3])
+    .then((response) => { console.log(response); })
+    .catch((error) => { console.warn(error); });
 };
 
-waiter();
+// Los pedidos también llegan aleatoriamente, como en la vida real :V
+setTimeout(() => { waiter(); }, randomTime(1000, 2000));
