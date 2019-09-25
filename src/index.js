341553215ac571445ab1e577e30874e80f78a1e1
BereSoto <beresoto.f@gmail.com>
2019-09-25 17:49:08 -0500

    reto 3

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time} es para la ${table}`);
+      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms es para la ${table}`);
     }, time);
   });
 }
 function randomTime(){
   const minTime = 1000
   const maxTime = 8000
-  return Math.round(Math.random() * (maxTime - minTime) + minTime )
+  let result = Math.round(Math.random() * 10000)
+  result = Math.max(minTime, result)
+  result = Math.min(maxTime, result)
+  return result
 }
 
 const menu = {
@@ -27,26 +30,28 @@ const waiter = () => {
 };
 
 const waiter2 = () => {
-  orders(randomTime(), menu.hotdog, [0])
-   .then((res) => console.log(res))
-   .catch((err) => console.log(err));
-  orders(randomTime(), menu.pizza,[2])
-   .then((res) => console.log(res))
-   .catch((err) => console.log(err));
-   return waiter3()
+  return orders(randomTime(), menu.hotdog, table[0])
+   .then((res) => {
+      console.log(res)
+      return orders(randomTime(), menu.pizza, table[2])
+   })
+   .then((res) => {
+    console.log(res)
+  })
  }
 
 const waiter3 = async () => {
- try{
-  const order4 = await orders(randomTime(), menu.hotdog, table[1])
-  const order3 = await orders(randomTime(), menu.pizza, table[1])
-  const orders = await orders(randomTime(), menu.hotdog, table[1])
-  console.log(order4)
- } 
- catch(err){
-   console.error(err)
+  try{
+    const productsTable = [menu.hotdog, menu.pizza, menu.hotdog] 
+    const promises = productsTable.map((product) => orders(randomTime(), product, table[1]))
+    const resultOrders = await Promise.all(promises)
+    console.log(resultOrders)
+  } 
+  catch(err){
+    console.error(err)
+  }
  }
-
-}
-
+ 
 waiter();
+waiter2();
+waiter3();
