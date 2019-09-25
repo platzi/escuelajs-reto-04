51e2cda9a12ea60a18f3ca6e1a39b69d3c36ef20
asilvabe <asilvabe24@gmail.com>
2019-09-24 22:43:55 -0500

    Problem 1

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
+const randomTime = () => Math.floor(Math.random() * (8000 - 1000 + 1) + 1000);
+
 const orders = (time, product, table) => {
-  console.log(`### Orden: ${product} para ${table}`);
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
-    }, time);
-  });
+    console.log(`### Orden: ${product} para ${table}`);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
+        }, time);
+    });
 }
 
 const menu = {
-  hamburger: 'Combo Hamburguesa',
-  hotdog: 'Combo Hot Dogs',
-  pizza: 'Combo Pizza',
+    hamburger: 'Combo Hamburguesa',
+    hotdog: 'Combo Hot Dogs',
+    pizza: 'Combo Pizza',
 };
 
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+    orders(randomTime(), menu.hamburger, table[3])
+        .then((res) => console.log(res))
+        .catch((err) => console.error(err));
 };
 
 waiter();
