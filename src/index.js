1ffa009441d31fe9a2e04443394ec8cf78cd8510
Rafael Niquefa <niquefa@gmail.com>
2019-09-25 21:21:45 -0500

    Finished

--- src/index.js
+++ src/index.js
@@ -7,14 +7,11 @@ function randomTime() {
 const orders = (oneOrderItem) => {
   console.log(`### Orden: ${oneOrderItem.product} para ${oneOrderItem.table}. por mesero ${oneOrderItem.waiter}. Tiempo estimado ${oneOrderItem.time}`);
   return new Promise((resolve, reject) => {
-    if (time <= 0) {
-      reject(oneOrderItem)
-    } else {
       setTimeout(() => {
-        resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table} atendido por mesero ${waiter}`);
-      }, time);
+        resolve(`=== Pedido servido: ${oneOrderItem.product}, tiempo de preparación ${oneOrderItem.time}ms para la ${oneOrderItem.table} atendido por mesero ${oneOrderItem.waiter}`)
+      }, oneOrderItem.time);
     }
-  });
+  );
 }
 
 const menu = {
@@ -22,20 +19,6 @@ const menu = {
   hotdog: 'Combo Hot Dogs',
   pizza: 'Combo Pizza',
 };
-
-const table = ['', 'Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
-
-const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
-};
-
-//waiter();
-
-function onError(key) {
-  console.log(`Algún error ocurriócon la orden del mesero ${key.waiter} de producto ${key.product}, para la mesa ${key.table}, tiempo estimado : ${key.time}`)
-}
 
 class OrderItem {
   constructor(time, product, table, waiter) {
@@ -46,28 +29,57 @@ class OrderItem {
   }
 }
 
-//Reserved word async to use await
-async function processOrders() {
-  const timeArray = [6000, randomTime(), randomTime(), randomTime(), randomTime(), randomTime()]
-  const productArray = [menu.hamburger, menu.hotdog, menu.pizza, menu.hotdog, menu.pizza, menu.hotdog]
-  const tableArray = [table[3], table[1], table[3], table[2], table[2], table[2]]
-  const waiters = ['waiter1', 'waiter2', 'waiter2', 'waiter3', 'waiter3', 'waiter3']
-  let keys = new Array();
-  for (let i = 0; i < timeArray.length; i++) {
-    keys.push(new OrderItem(timeArray[i], productArray[i], tableArray[i], waiters[i]));
-  }
+function onError(key) {
+  console.log(`Algún error ocurrió con la orden del mesero ${key.waiter} de producto ${key.product}, para la mesa ${key.table}, tiempo estimado : ${key.time}`)
+}
+const table = ['', 'Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
+
+const timeArrayFirstOrders = [6000, randomTime(), randomTime()]
+const productArrayFirstOrders = [menu.hamburger, menu.hotdog, menu.pizza]
+const tableArrayFirstOrders = [table[3], table[1], table[3]]
+const waitersFirstOrders = ['waiter1', 'waiter2', 'waiter2']
+
+var orderskeysFirstOrders = new Array();
+for (let i = 0; i < timeArrayFirstOrders.length; i++) {
+  orderskeysFirstOrders.push(new OrderItem(timeArrayFirstOrders[i], productArrayFirstOrders[i], tableArrayFirstOrders[i], waitersFirstOrders[i]));
+}
+
+const waiter = () => {
+  orders(new OrderItem(timeArrayFirstOrders[0], productArrayFirstOrders[0], tableArrayFirstOrders[0], waitersFirstOrders[0]))
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+};
+waiter();
+
+const waiter2 = () => {
+  orders(new OrderItem(timeArrayFirstOrders[1], productArrayFirstOrders[1], tableArrayFirstOrders[1], waitersFirstOrders[1])) 
+  .then( theOrder1 => {
+    console.log(theOrder1)
+    return orders(new OrderItem(timeArrayFirstOrders[2], productArrayFirstOrders[2], tableArrayFirstOrders[2], waitersFirstOrders[2]))
+  })
+  .then( theOrder2 => {
+    console.log(theOrder2)
+  })
+  .catch(onError)
+};
+waiter2();
+
+async function waiter3(keys) {
   let promises = keys.map(key => orders(key))
   try {
-    //await forces to fullfill all promises before continue
-    //await must be inside a try catch block
     let theOrders = await Promise.all(promises)
-    console.log(theOrders)
     for (let i = 0; i < theOrders.length; i++) {
-      console.log(theOrders[i])
+       console.log(theOrders[i])
     }
   } catch (key) {
     onError(key)
   }
 }
 
-processOrders()
+const timeArrayWaiter3 = [randomTime(), randomTime(), randomTime()]
+const productArrayWaiter3 = [menu.hotdog, menu.pizza, menu.hotdog]
+let orderskeys = new Array();
+for (let i = 0; i < timeArrayWaiter3.length; i++) {
+  orderskeys.push(new OrderItem(timeArrayWaiter3[i], productArrayWaiter3[i], table[2], 'waiter3'));
+}
+waiter3(orderskeys)
