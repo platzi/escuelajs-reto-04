50269532f3a81472191b254f15fd8339d6db37ea
Solej77 <jose_luispp7@hotmail.com>
2019-09-26 12:11:39 -0500

    First Problem: Add function getRandomTime

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+let getRandomTime = () => {
+  min = Math.ceil(1);
+  max = Math.floor(8)
+  return (Math.floor(Math.random() * (max - min + 1)) + min)*1000;
+
+}
+
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
@@ -16,7 +23,8 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  let time = getRandomTime();
+  orders(time, menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
