a8e683d900b123e76ac80c59c2e65b51a6366e7a
Carlos Perez <carlos@ioncom.com.mx>
2019-09-26 13:23:08 -0500

    se agregan comentarios al codigo y se actualiza el archivo PULL_REQUEST_TEMPLATE.md

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(
-        `=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`
-      );
+      try {
+        resolve(
+          `=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`
+        )
+      } catch (error) {
+        reject(
+          `=== ${error} Erro en el pedido: ${product}, para la ${table}`
+        )
+      }
     }, time);
   });
 };
@@ -19,7 +25,15 @@ const menu2 = ['Combo Hamburguesa', 'Combo Hot Dogs', 'Combo Pizza'];
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 const tableWaiter2 = ['Mesa 1', 'Mesa 3'];
 
+
+/* ============== Reto 1 ==================== */
+/*
+ Se agrega la función getRandomTime para obtener el tiempo de
+ preparación de la orden.
+ */
+
 const waiter = () => {
+  console.log('Reto 1')
   orders(6000, menu.hamburger, table[3])
     .then(res => console.log(res))
     .catch(err => console.error(err));
@@ -32,7 +46,11 @@ function getRandomTime() {
   return randomTime;
 }
 
-/* Reto 2 */
+/* ============== Reto 2 ==================== */
+/*
+ Se agrega la función getRandomOrden, getRandomTable para obtener
+ la orden en que se atienden las mesas y la orden de los clientes
+ */
 
 function getRandomOrden() {
   min = 0;
@@ -49,9 +67,9 @@ function getRandomTable() {
 }
 
 const waiter2 = () => {
+  console.log('Reto 2')
   orders(getRandomTime(), getRandomOrden(), getRandomTable())
     .then(res => {
-      /* console.log('Primera orden', tableWaiter2[randomTable]);*/
       console.log(res);
       if (tableWaiter2[randomTable] == 'Mesa 1') {
         randomTable = 1;
@@ -65,18 +83,23 @@ const waiter2 = () => {
       );
     })
     .then(res => {
-      //console.log('Segunda orden', tableWaiter2[randomTable]);
       console.log(res);
     })
     .catch(err => console.error(err));
 };
 
-/* Reto 3 */
+/* ============== Reto 3 ==================== */
+/*
+ Se agrega la función numberPeople, takeOrders y cookOrders para obtener
+ el número de personas que se sientan en la mesa, la toma de orden y la 
+ preparación de las órdenes
+ */
 
 function numberPeople() {
   min = 1;
   max = 4;
   randomPeople = Math.floor(Math.random() * (max - min)) + min;
+  console.log(`Numero de personas en la mesa: ${randomPeople}`)
   return randomPeople;
 }
 
@@ -104,11 +127,17 @@ async function cookOrder(item) {
 
 function order(item) {
   let timeCook = getRandomTime();
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(
-        `=== Pedido servido: ${item}, tiempo de preparación ${timeCook}ms`
-      );
+      try {
+        resolve(
+          `=== Pedido servido: ${item}, tiempo de preparación ${timeCook}ms`
+        )
+      } catch (error) {
+        reject(
+          `=== ${error} Erro en el pedido: ${item}`
+        )
+      }
     }, timeCook);
   });
 }
@@ -117,7 +146,9 @@ function onError(error) {
   console.log(`Ocurrio el sigueinte error: ${error}`);
 }
 
+
 const waiter3 = async () => {
+  console.log('Reto 3')
   try {
     let infoPeople = await numberPeople();
     let ordersPeople = await takeOrders(infoPeople, table[1]);
@@ -128,5 +159,5 @@ const waiter3 = async () => {
 };
 
 //waiter();
-waiter2();
-//waiter3();
+//waiter2();
+waiter3();
