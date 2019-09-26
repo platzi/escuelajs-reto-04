2e7e8c60a73eb101a5d172fd9a5a19a5524781bc
César Hansy Dueñas Barragán <cesarhansy@gmail.com>
2019-09-25 21:36:27 -0500

    Refactor randomTime and use it. Solve Challenge 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-function randomTime(){
-  return Math.ceil(Math.random() * 8) * 1000
+const randomTime = () => {
+ return Math.ceil(Math.random() * 8) * 1000
 }
 
-const orders = (time, product, table) => {
+
+const orders = (time, product, table) => {  
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -20,7 +21,7 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(randomTime, menu.hamburger, table[3])
+  orders(randomTime(), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
