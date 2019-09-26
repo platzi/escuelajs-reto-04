8721520bd734b4750bc2cf321187c959d583f0c1
Carlos Perez <carlos.perez@charliegperez.com>
2019-09-26 02:26:12 -0500

    ajustes del reto 3

--- src/index.js
+++ src/index.js
@@ -16,9 +16,7 @@ const menu = {
 };
 
 const menu2 = ['Combo Hamburguesa', 'Combo Hot Dogs', 'Combo Pizza'];
-
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
-
 const tableWaiter2 = ['Mesa 1', 'Mesa 3'];
 
 const waiter = () => {
@@ -34,12 +32,12 @@ function getRandomTime() {
   return randomTime;
 }
 
+/* Reto 2 */
+
 function getRandomOrden() {
   min = 0;
   max = 3;
   randomOrden = Math.floor(Math.random() * (max - min)) + min;
-  console.log(randomOrden)
-  console.log(menu2[randomOrden])
   return menu2[randomOrden];
 }
 
@@ -47,21 +45,18 @@ function getRandomTable() {
   min = 0;
   max = 2;
   randomTable = Math.floor(Math.random() * (max - min)) + min;
-  console.log('randomTable :', randomTable);
   return tableWaiter2[randomTable];
 }
 
 const waiter2 = () => {
   orders(getRandomTime(), getRandomOrden(), getRandomTable())
     .then(res => {
-      /* console.log('Primera orden', tableWaiter2[randomTable]);
-      console.log(res); */
+      /* console.log('Primera orden', tableWaiter2[randomTable]);*/
+      console.log(res);
       if (tableWaiter2[randomTable] == 'Mesa 1') {
         randomTable = 1;
-        /* console.log('if ', tableWaiter2[randomTable]); */
       } else {
         randomTable = 0;
-        /* console.log('else ', tableWaiter2[randomTable]); */
       }
       return orders(
         getRandomTime(),
@@ -70,18 +65,18 @@ const waiter2 = () => {
       );
     })
     .then(res => {
+      //console.log('Segunda orden', tableWaiter2[randomTable]);
       console.log(res);
     })
     .catch(err => console.error(err));
 };
 
+/* Reto 3 */
 
-
-function numberPeople(){
+function numberPeople() {
   min = 1;
   max = 4;
   randomPeople = Math.floor(Math.random() * (max - min)) + min;
-  console.log('randomPeople :', randomPeople);
   return randomPeople;
 }
 
@@ -91,26 +86,47 @@ const takeOrders = (infoPeople, table) => {
     let product = getRandomOrden();
     arrayOrders[i] = product;
     console.log(`### Orden: ${product} para ${table}`);
-    //const element = array[index];
   }
   return arrayOrders;
+};
+
+const cookOrders = async (ordersPeople, table) => {
+  const promises = ordersPeople.map(cookOrder);
+  await Promise.all(promises);
+  console.log(`Se entregan ordenes de la mesa ${table}`);
+};
+
+async function cookOrder(item) {
+  await order(item).then(res => {
+    console.log(res);
+  });
+}
+
+function order(item) {
+  let timeCook = getRandomTime();
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(
+        `=== Pedido servido: ${item}, tiempo de preparación ${timeCook}ms`
+      );
+    }, timeCook);
+  });
 }
 
 function onError(error) {
   console.log(`Ocurrio el sigueinte error: ${error}`);
 }
 
-const waiter3 = async() => {
+const waiter3 = async () => {
   try {
     let infoPeople = await numberPeople();
     let ordersPeople = await takeOrders(infoPeople, table[1]);
-    console.log("ordenes ", ordersPeople);
-    //orders(getRandomTime(), getRandomOrden(), table[1]);
+    await cookOrders(ordersPeople, table[1]);
   } catch (error) {
     onError(error);
   }
-}
+};
 
 //waiter();
-//waiter2();
-waiter3();
+waiter2();
+//waiter3();
