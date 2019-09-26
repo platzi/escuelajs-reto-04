5f3c83aa901de6925ad768cd234588defd945e77
Rodrigo Ortiz <rodrigortizm@gmail.com>
2019-09-26 06:58:29 -0500

    reto opcional casi solucionado

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+var API = 'https://us-central1-escuelajs-api.cloudfunctions.net/orders';
+var xhttp = new XMLHttpRequest();
+
+function fetchData(url_api) {
+  return new Promise(function(resolve, reject) {
+    xhttp.onreadystatechange = function (event) {
+      if (xhttp.readyState === 4) {
+        if (xhttp.status === 200)
+          resolve(JSON.parse(xhttp.responseText));
+        else return reject(url_api);
+      }
+    };
+    xhttp.open('GET', url_api, false);
+    xhttp.send();
+  });
+};
+
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
-
     setTimeout(() => {
       if (true) {
         resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
       } else {
         reject(`!== Pedido falló: ${product}, tiempo de preparación ${time}ms para la ${table}`);
       }
-
     }, time);
   });
 }
@@ -40,22 +57,45 @@ const waiter2 = () => {
         .then((res) => console.log(res));
       console.log(res);
     })
-
     .catch((err) => console.error(err));
-
 };
 async function waiter3() {
-let pedido1 = await orders(randomTime(), menu.hotdog, table[1]);
-let pedido2 = await orders(randomTime(), menu.pizza, table[1]);
-let pedido3 = await orders(randomTime(), menu.hotdog, table[1]);
-console.log(pedido1);
-console.log(pedido2);
-console.log(pedido3);
+    try {
+      let pedido1 = await orders(randomTime(), menu.hotdog, table[1]);
+      let pedido2 = await orders(randomTime(), menu.pizza, table[1]);
+      let pedido3 = await orders(randomTime(), menu.hotdog, table[1]);
+      console.log(pedido1);
+      console.log(pedido2);
+      console.log(pedido3);
+    } catch (error) {
+      //errorCallBack(error);
+      console.log(error);
+    }
 };
+async function waiter4(){
+  try{
+    let orden1 = await fetchData(API);
+    let p1 = await orders(randomTime(), orden1.data, table[4]);
+    let orden2 = await fetchData(API);
+    let p2 = await orders(randomTime(), orden2.data, table[4]);
+    let orden3 = await fetchData(API);
+    let p3 = await orders(randomTime(), orden3.data, table[4]);
+    let orden4 = await fetchData(API);
+    let p4 = await orders(randomTime(), orden4.data, table[4]);     
+    console.log(p1);
+    console.log(p2);
+    console.log(p3);
+    console.log(p4);
+  } catch (error) {
+    //errorCallBack(error);
+    console.log(error);
+  }
 
+}
 
 waiter();
 waiter2();
 waiter3();
+waiter4();
 
 
