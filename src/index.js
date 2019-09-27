31c735559db9ef031a41908526b61091ffe1e028
saforus <1405pablo@gmail.com>
2019-09-26 23:16:03 -0500

    RETO 4, 4 ejercicio resuelto

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 const TIEMPO1 = 1000;
 const TIEMPO2 = 8000;
 
+const menu = {
+  hamburger: "Combo Hamburguesa",
+  hotdog: "Combo Hot Dogs",
+  pizza: "Combo Pizza"
+};
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
+    if (
+      product != "Combo Hamburguesa" &&
+      product != "Combo Hot Dogs" &&
+      product != "Combo Pizza"
+    ) {
+      resolve(
+        `=== El ${product}, no lo tenemos disponible en este momento, le ofrecemos una disculpa.`
+      );
+    }
+
     setTimeout(() => {
       resolve(
         `=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`
       );
     }, time);
   });
-};
-
-const menu = {
-  hamburger: "Combo Hamburguesa",
-  hotdog: "Combo Hot Dogs",
-  pizza: "Combo Pizza"
 };
 
 const table = ["Mesa 1", "Mesa 2", "Mesa 3", "Mesa 4", "Mesa 5"];
@@ -30,18 +39,6 @@ const waiter = () => {
     .catch(err => console.error(err));
 };
 
-// waiter();
-
-// const waiter2 = () => {
-//   const tiempo = numAleatorio(TIEMPO1, TIEMPO2);
-//   orders(tiempo, menu.hamburger, table[0])
-//     .then(res => console.log(res))
-//     .then(
-//       orders(tiempo, menu.hamburger, table[2]).then(res => console.log(res))
-//     )
-//     .catch(err => console.error(err));
-// };
-
 const waiter2 = () => {
   const tiempo = numAleatorio(TIEMPO1, TIEMPO2);
   orders(tiempo, menu.hamburger, table[0]).then(function(res1) {
@@ -51,21 +48,6 @@ const waiter2 = () => {
     });
   });
 };
-
-//waiter2();
-
-// const waiter3 = () => {
-//   const tiempo = numAleatorio(TIEMPO1, TIEMPO2);
-//   orders(tiempo, menu.hamburger, table[0])
-//     .then(res => console.log(res))
-//     .then(
-//       orders(tiempo, menu.hamburger, table[1]))
-//     )
-//     .then(
-//       orders(tiempo, menu.hamburger, table[1]).then(res => console.log(res))
-//     )
-//     .catch(err => console.error(err));
-// };
 
 async function waiter3() {
   let response1 = await orders(
@@ -84,11 +66,48 @@ async function waiter3() {
     table[1]
   );
 
-  return console.log(
-    `Servi pedidio mesa2 ${response1} + \n, rServi pedidio mesa ${response2} + "\n",Servi pedido mesa3 ${response3}`
-  );
-  // let last = await Promise.all([response1, response2, response3]);
-  // return console.log(last[0] + "\n", last[1] + "\n", last[2]);
+  return console.log(` ${response1} + \n ${response2} + \n ${response3}`);
 }
 
+async function waiter4() {
+  const response = await consultaAPI().then(data => data);
+
+  const PEDIDO1 = Object.values(response[0]);
+  const PEDIDO2 = Object.values(response[1]);
+  const PEDIDO3 = Object.values(response[2]);
+  const PEDIDO4 = Object.values(response[3]);
+
+  let orden1 = await orders(numAleatorio(TIEMPO1, TIEMPO2), PEDIDO1, table[4]);
+  let orden2 = await orders(numAleatorio(TIEMPO1, TIEMPO2), PEDIDO2, table[4]);
+  let orden3 = await orders(numAleatorio(TIEMPO1, TIEMPO2), PEDIDO3, table[4]);
+  let orden4 = await orders(numAleatorio(TIEMPO1, TIEMPO2), PEDIDO4, table[4]);
+
+  let last = await Promise.all([orden1, orden2, orden3, orden4]);
+  return console.log(last[0] + "\n", last[1] + "\n", last[2] + "\n", last[3]);
+}
+
+async function consultaAPI() {
+  const url = `https://us-central1-escuelajs-api.cloudfunctions.net/orders`;
+
+  const fetch = require("node-fetch");
+  let pedido1 = await fetch(url).then(function(response) {
+    return response.json();
+  });
+  let pedido2 = await fetch(url).then(function(response) {
+    return response.json();
+  });
+  let pedido3 = await fetch(url).then(function(response) {
+    return response.json();
+  });
+  let pedido4 = await fetch(url).then(function(response) {
+    return response.json();
+  });
+
+  let last = await Promise.all([pedido1, pedido2, pedido3, pedido4]);
+  return last;
+}
+
+waiter();
+waiter2();
 waiter3();
+waiter4();
