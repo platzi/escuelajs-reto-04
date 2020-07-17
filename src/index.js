79029d0ffaf69b7ad31f87c9be72014228a9b6d9
brahianf <brahianf@unicauca.edu.co>
2020-07-17 17:28:29 -0500

    Cuarto Problema

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const fetchData = require('./utils/fetchData.js');
+const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/orders/';
+
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
@@ -53,6 +56,28 @@ const waiter3 = async () => {
   }
 }
 
+const waiter4 = async (url_api) => {
+  try {
+    var time=randomTime(1000,8000);
+    const data = await fetchData(url_api);
+    console.log(data)
+    const order1 = await orders(time, data.data, table[4])
+    var time=randomTime(1000,8000);
+    const order2 = await orders(time, data.data, table[4])
+    var time=randomTime(1000,8000);
+    const order3 = await orders(time, data.data, table[4])
+    var time=randomTime(1000,8000);
+    const order4 = await orders(time, data.data, table[4])
+    Promise.all([order1,order2,order3,order4])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 waiter();
 waiter2();
 waiter3();
+waiter4(API);
