e45dfea064195b19b9f20d78e3a28a8f69ab44ef
juliodev <juliocesardeveloper@gmail.com>
2020-07-29 11:38:17 -0500

    Resuelto el problema 3

--- src/index.js
+++ src/index.js
@@ -12,9 +12,13 @@ const orders = (randomTime, product, table) => {
   });
 }
 
+
+//Solución al problema 1
 const randomTime = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 }
+//fin de solución problema 1
+
 
 const menu = {
   hamburger: 'Combo Hamburguesa',
@@ -32,6 +36,7 @@ const waiter = () => {
 
 waiter();
 
+//Solución problema 2
 const waiter2 = () => {
   orders(randomTime(1000, 8001), menu.hotdog, table[0])
   .then((res) => console.log(res))
@@ -41,3 +46,21 @@ const waiter2 = () => {
 }
 
 waiter2();
+//fin de solución problema 2
+
+//Solución problema 3
+const waiter3 = async () => {
+  try {
+    const pedido1Mesa2 = await orders(randomTime(1000, 8001), menu.hotdog, table[1]);
+    const pedido2Mesa2 = await orders(randomTime(1000, 8001), menu.pizza, table[1]);
+    const pedido3Mesa2 = await orders(randomTime(1000, 8001), menu.hotdog, table[1]);
+    console.log(pedido1Mesa2);
+    console.log(pedido2Mesa2);
+    console.log(pedido3Mesa2);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+waiter3();
+//Fin solución problema 3
