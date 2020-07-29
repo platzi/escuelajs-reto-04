8bdd7ed353002ec9b47a4e07d18d1e4be02f3d46
julioDev <juliocesardeveloper@gmail.com>
2020-07-28 22:15:43 -0500

    Primer problema resuelto

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const orders = (randomTime, product, table) => {
+  console.log(`### Orden: ${product} para ${table}`);
+  return new Promise((resolve, reject) => {
+    if (true) {
+      setTimeout(() => {
+        resolve(`=== Pedido servido: ${product}, tiempo de preparación ${randomTime}ms para la ${table}`);
+      }, randomTime);
+    } else {
+      reject('Error');
+    }
+
+  });
+}
+
+const randomTime = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const menu = {
+  hamburger: 'Combo Hamburguesa',
+  hotdog: 'Combo Hot Dogs',
+  pizza: 'Combo Pizza',
+};
+
+const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
+
+const waiter = () => {
+  orders(randomTime(1000, 8001), menu.hamburger, table[3])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+};
+
+waiter();
