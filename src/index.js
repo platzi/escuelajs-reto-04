b7bf10d6acec5aa1374d488d7a219fa97f38cee6
Walter Salas <soy@waltersalas.com>
2020-08-05 15:37:12 -0500

    Second challenge solved

--- src/index.js
+++ src/index.js
@@ -1,30 +1,45 @@
 const orders = (time, product, table) => {
-  console.log(`### Orden: ${product} para ${table}`);
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
-      reject(error = new Error(`Error ${error}`))
-    }, time);
-  });
-}
-
-const randomTime = (min,max) => {
-  return Math.floor(Math.random() * (min - max) + min);
-}
-
-const menu = {
-  hamburger: 'Combo Hamburguesa',
-  hotdog: 'Combo Hot Dogs',
-  pizza: 'Combo Pizza',
+    console.log(`### Orden: ${product} para ${table}`);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(
+                `=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`
+            );
+            reject(new Error("Error"));
+        }, time);
+    });
 };
 
-const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
+const randomTime = (min, max) => {
+    return Math.floor(Math.random() * (min - max) + min);
+};
+
+const menu = {
+    hamburger: "Combo Hamburguesa",
+    hotdog: "Combo Hot Dogs",
+    pizza: "Combo Pizza",
+};
+
+const table = ["Mesa 1", "Mesa 2", "Mesa 3", "Mesa 4", "Mesa 5"];
 
 const waiter = () => {
-  let time = randomTime(1000,8000)
-  orders(time, menu.hamburger, table[3])
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+    let time = randomTime(1000, 8000);
+    orders(time, menu.hamburger, table[3])
+        .then((res) => console.log(res))
+        .catch((err) => console.error(err));
+};
+
+const waiter2 = () => {
+    let time = randomTime(1000, 8000);
+    orders(time, menu.hamburger, table[0])
+        .then((res) => console.log(res));
+
+    let time2 = randomTime(1000, 8000);
+    orders(time2, menu.pizza, table[2])
+        .then((res) => console.log(res))
+
+        .catch((err) => console.error(err));
 };
 
 waiter();
+waiter2();
