afb244fb2606c60a96513b04a3a49627bd7331e7
Sergio Hortua <sergioandresjsv@gmail.com>
2020-09-23 13:48:12 -0500

    Problema 1 solucion ok

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
+    if(time<9000){
+      setTimeout(() => {
       resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
     }, time);
+  }else{
+    const error= new Error('Sobrepasaste mi paciencia')
+    console.log(`Tu tiempo fue de: ${time}😞`);
+    reject(error)
+  }
   });
 }
 
@@ -16,9 +23,20 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(randomTime(), menu.hamburger, table[3])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+};
+const waiter2 = () => {
+  orders(randomTime(), menu.hotdog, table[0])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+  orders(randomTime(), menu.pizza, table[2])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
 
+const randomTime = () => Math.floor(Math.random() * (8000 - 100+ 1) + 1000)
+
 waiter();
+waiter2();
