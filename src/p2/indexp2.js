fec366d0b6a06ae2a85d811d09ee66293df7ef35
Sergio Hortua <sergioandresjsv@gmail.com>
2020-09-23 14:31:01 -0500

    Problema 2 finish

--- src/p2/indexp2.js
+++ src/p2/indexp2.js
@@ -1,6 +1,6 @@
 
 const orders = (time, product, table) => {
-    console.log(`*** Orden: ${product} para ${table} ***`);
+    console.log(`*** Pedido ${table}:  ${product} ***`);
     return new Promise((resolve, reject) => {
       if(time<9000){
         setTimeout(() => {
@@ -21,12 +21,8 @@ const orders = (time, product, table) => {
   };
   
   const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
+  const randomTime = () => Math.floor(Math.random() * (8000 - 100+ 1) + 1000)
   
-  const waiter = () => {
-    orders(randomTime(), menu.hamburger, table[3])
-      .then((res) => console.log(res))
-      .catch((err) => console.error(err));
-  };
   const waiter2 = () => {
     orders(randomTime(), menu.hotdog, table[0])
       .then((res) => console.log(res))
@@ -36,8 +32,5 @@ const orders = (time, product, table) => {
       .catch((err) => console.error(err));
   };
   
-  const randomTime = () => Math.floor(Math.random() * (8000 - 100+ 1) + 1000)
-  
-  waiter();
   waiter2();
   
